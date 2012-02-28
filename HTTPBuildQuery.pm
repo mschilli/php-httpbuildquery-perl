d23941ddeacbabd5eeb47aa51cf2dc3e92cbbb98
mschilli <github@perlmeister.com>
2012-02-27 19:07:50 -0800

    Requiring perl-5.8 to avoid utf8 problems with 5.6.

--- HTTPBuildQuery.pm
+++ HTTPBuildQuery.pm
@@ -13,7 +13,7 @@ require Exporter;
 our @ISA = qw(Exporter);
 our @EXPORT_OK = qw(http_build_query http_build_query_utf8);
 
-our $VERSION = "0.04";
+our $VERSION = "0.05";
 
 ###########################################
 sub http_build_query {
